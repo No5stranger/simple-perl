1921c23822f8ea0a584250e87bf4605beb3e2cbd
cjp <No5stranger@gmail.com>
2020-03-29 22:36:36 +0800

    sort

--- read_file.pl
+++ read_file.pl
@@ -1,17 +1,24 @@
 #!/usr/bin/perl
 
 my $total = 0;
+my %out_data;
 open(DATA, "<data.log") or die "data.log open filed";
 while(<DATA>) {
     my @spl = split(',', $_);
     $total += $spl[1];
     if ($spl[1] > 3) {
-        printf "%d,%s\n", $spl[1], $spl[0];
+        $out_data{$spl[0]} = $spl[1]
     }
 }
+foreach my $k (sort {$out_data{$a} <=> $out_data{$b}} keys %out_data) {
+    printf "%s,%d\n", $k, $out_data{$k};
+}
+#while (my ($k, $v) = sort {{$out_data{$a}} <=> $out_data{$b}} each %out_data) {
+#    printf "%s,%d\n", $k, $v;
+#}
 open(WRITE_DATA, ">result.log");
 $output = sprintf("total count: %d", $total);
 print $output;
-print WRITE_DATA $output;
+#print WRITE_DATA $output;
 close(DATA);
 close(WRITE_DATA);
